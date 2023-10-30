978a80634e427cb6e0450254bd37b0dc0b2b650e
yayangIzza <149374422+yayangIzza@users.noreply.github.com>
2023-10-30 19:41:19 +0700

    Add files via upload

---
+++ lingkaran.php
@@ -0,0 +1,23 @@
+<?php
+
+echo "Nama  : Yayang Izzatul Haq<br>";
+echo "NIM   : 22205052<br>";
+echo "Kelas : PBO<br>";
+
+
+echo "=====perhitungan lingkaran=====<br>";
+echo "masukkan nilai diameter lingkaran : 100<br>";
+
+echo "=====hasil perhitungan lingkaran=====<br>";
+$jari_jari = "50 cm<br>";
+$luas_keliling = "7853,98 cm<br>";
+$keliling_lingkaran = "314,16 cm<br>";
+
+echo "jari-jari lingkaran = 50 cm<br>";
+echo "luas lingkaran = 7853,98 cm<br>";
+$hasil = $jari_jari * $luaske_liling;
+$keliling_lingkaran = $hasil; 
+echo "keliling lingkaran = $hasil<br>";
+echo "keliling lingkaran = 314,16 cm";
+
+?>
